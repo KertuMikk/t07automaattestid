716d90fa0391da8bbaf8fd5f4fb3d4f18f3c9ccd
Kertu Mikk <kertumik@tlu.ee>
2018-04-05 08:22:04 +0300

    liides

---
+++ Kodunetoo7/src/main/java/com/mycompany/kodunetoo7/ruumalaLiides.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.kodunetoo7;
+
+
+public interface ruumalaLiides {
+    public double Ruumala1(int külg1, int külg2, int kõrgus);
+    public double Pindala(int külg1, int külg2, int kõrgus);
+}
